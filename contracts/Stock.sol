9d399a629c44640f51cddfc9c587a9f21a832534
glody007 <glodymbutwile@gmail.com>
2021-11-16 16:12:12 +0100

    test sending stocks

--- contracts/Stock.sol
+++ contracts/Stock.sol
@@ -4,27 +4,35 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Stock {
+  string public functionCalled;
   string public name = "Company Stock Option";
   string public symbol = "CSO";
   uint public totalSupply = 1000000;
+  uint public totalEther = 0;
   address public minter;
   mapping(address => uint) balances;
+  address payable[] addressOwners;
 
   event Sent(address from, address to, uint amount);
 
   constructor() {
     minter = msg.sender;
     balances[minter] = totalSupply;
+    addressOwners.push(payable(minter));
   }
 
   error InsufficientBalance(uint requested, uint available);
 
-  function send(address receiver, uint amount) public {
+  function sendStockTo(address receiver, uint amount) public {
        if (amount > balances[msg.sender])
            revert InsufficientBalance({
                requested: amount,
                available: balances[msg.sender]
            });
+
+      if (balances[receiver]  == 0) {
+        addressOwners.push(payable(receiver));
+      }
 
        balances[msg.sender] -= amount;
        balances[receiver] += amount;
@@ -41,5 +49,19 @@ contract Stock {
 
   function addresse() external view returns (address) {
     return msg.sender;
+  }
+
+  function sendEtherTo(address payable receiver, uint amountEther) public {
+    receiver.transfer(amountEther);
+  }
+
+  function sendEther() external payable {
+    functionCalled = 'sendEther';
+  }
+
+  function distribute() external {
+    for (uint i=0; i < addressOwners.length; i++) {
+      sendEtherTo(addressOwners[i], address(this).balance * balances[addressOwners[i]]/totalSupply);
+    }
   }
 }
--- test/stock-test.js
+++ test/stock-test.js
@@ -7,10 +7,17 @@ describe("Stock", function () {
     const stock = await Stock.deploy();
     await stock.deployed();
 
+    const addressSecondOwner = "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65"
+
     const balanceMinter = await stock.stockQte()
     expect(balanceMinter.toNumber() === 1000000)
 
-    const balanceOneOwner = await stock.stockQteOf("0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65")
-    expect(balanceOneOwner.toNumber() === 0)
+    const balanceSecondOwner = await stock.stockQteOf(addressSecondOwner)
+    expect(balanceSecondOwner.toNumber() === 0)
+
+    await stock.sendStockTo(addressSecondOwner, 500000)
+    const balanceSecondOwnerAfter = await stock.stockQteOf(addressSecondOwner)
+    expect(balanceSecondOwnerAfter.toNumber() === 500000)
+
   });
 });
